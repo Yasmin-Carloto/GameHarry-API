2818aff724cefb1ee410a21f394c064921e36e35
Ingryd <ingrydcduarte@gmail.com>
2024-07-03 08:56:51 -0300

    Initial Commit

---
+++ gameHarry-API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gameHarry-API
+//
+//  Created by Ingryd Cordeiro Duarte on 03/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
