2fc5a0249fa2ae847c4767517543f3f9ca704320
Ingryd <ingrydcduarte@gmail.com>
2024-07-03 09:56:34 -0300

    add apiservice.swift

---
+++ APIServices/apiservice.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class ApiServices{
+    private let baseURL = ""
+    private let api = ApiServices()
+    private init(){}
+    
+    //selHousesClosure
+    func selHouses(code: String, completion: @escaping (_data: Data?) -> Void){
+    //code
+    }
+}
