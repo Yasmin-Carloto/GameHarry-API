ee88596cdf8b0abbb34bf2e525f05d3b92464f97
Luciana Adrião <user@10-45-51-200.0200.ifce.br.iosda.org>
2024-07-04 09:49:42 -0300

    created LandingPage

---
+++ gameHarry-API/Views/LandingPage.swift
@@ -0,0 +1,18 @@
+//
+//  LandingPage.swift
+//  gameHarry-API
+//
+//  Created by User on 04/07/24.
+//
+
+import SwiftUI
+
+struct LandingPage: View {
+    var body: some View {
+        Text("This is going to be a Landing Page")
+    }
+}
+
+#Preview {
+    LandingPage()
+}
