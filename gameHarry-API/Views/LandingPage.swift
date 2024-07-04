51ac60a8cb053b713ebf91922b030d8cb0fc9bf5
Luciana Adrião <user@10-45-51-200.0200.ifce.br.iosda.org>
2024-07-04 11:22:48 -0300

    Folder organization

--- gameHarry-API/Views/LandingPage.swift
+++ gameHarry-API/Views/LandingPage.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct LandingPage: View {
+    
     var body: some View {
-        Text("This is going to be a Landing Page")
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    LazyVStack(alignment: .center, content: {
+                        ForEach(1...5, id: \.self) { count in
+                            RoundedRectangle(cornerRadius: 12.0)
+                                .frame(width: 80, height: 80)
+                            Text("Feitiços")
+                        }
+                    })
+                }
+            }
+            VStack {
+                Text("Seu ano em hogwarts").font(.title).bold()
+                RoundedRectangle(cornerRadius: 12.0)
+                    .padding()
+            }
+            .navigationTitle("Bem Vindo ao Mundo Mágico !")
+            .navigationBarTitleDisplayMode(.large)
+            .frame(alignment: .center)
+        }
     }
 }
 
